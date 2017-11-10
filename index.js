601e5429ab39575045b9bf3eb48a90cb0d15a189
Obi <obinnawalker@gmail.com>
2017-11-10 11:33:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
-let logDriverNames= function(driver){
-  driver.forEach(log){
+let logDriverNames = function(driver){
+  driver.forEach(function (log){
     console.log(log);
   });
 }
