d09582939680149defc2c76acda9f9d78f6edf3d
Obi <obinnawalker@gmail.com>
2017-11-10 11:54:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const logDriversByHometown = function(driver, location){
 const driversByRevenue = function(drivers){
   drivers.forEach(function(driver){
     let newDrivers = driver.revenue
-    newDrivers.sort(function(a,b){
+  //  newDrivers.sort(function(a,b){
     return a - b
   });
   return newDrivers
